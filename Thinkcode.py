68a221b5a409cc86d88acd180b886db232d6c2df
NathanLoranceMvb <matslavarock@gmail.com>
2021-11-26 09:15:01 +0100

    klaar

---
+++ Thinkcode.py
@@ -0,0 +1,20 @@
+def intro():
+    print ("Hello here I will show my top 3 games")
+
+def outro():
+    print ("thank you for checking this out hope you check these games out and find somthing fun to play")
+
+def top3():
+    print ("My number 3 would be the division 2 because of the action and the nice world building")
+
+def top2():
+    print ("My number 2 would be portal 2 its the first game I've played on my pc that I bought myself and its nostalgic and fun")
+
+def top1():
+    print ("My favorite game is just cause 3 the base libaration, grappeling and flying around is like no other game.")
+
+intro()
+top3()
+top2()
+top1()
+outro()
